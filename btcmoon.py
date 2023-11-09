f372b05ceef04e658e8d70e3e06639eb011645e0
Roger Garcia <96830104+Rogergarciatsa@users.noreply.github.com>
2023-11-09 11:30:01 +0000

    unwanted columns removed

--- btcmoon.py
+++ btcmoon.py
@@ -14,9 +14,18 @@ datetoday = dt.datetime.today().strftime("%m/%d/%Y")
 driver = webdriver.Chrome()
 driver.get('https://www.investing.com/crypto/bitcoin/historical-data')
 
+#click cookies
 CookiesAccept = driver.find_element(By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')
 CookiesAccept.click()
+
+#find BTC Price
 BTCpriceToday = driver.find_element(By.XPATH, '//*[@id="__next"]/div[2]/div[2]/div[2]/div[1]/div[1]/div[3]/div/div[1]/div[1]/div[1]')
+BTCpriceToday = BTCpriceToday.text
+
 print(BTCpriceToday)
 print(datetoday)
 print(dataFrame)
+
+new_row = pd.DataFrame({'Date':(datetoday), 'Price':(BTCpriceToday)}, index=[0])
+df2 = pd.concat([new_row,dataFrame.loc[:]]).reset_index(drop=True)
+print(df2)
